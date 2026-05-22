d2a58245f0f947e08abd2d82b828bd8bc5355c69
Khin Thiri Kyaw <khinthirikyaw8@gmail.com>
2026-05-22 23:57:40 +0630

    Add server upload/configuration support, auth middleware, and package updates
    Add imageKit.js and multer.js
    Add server controller and middleware support
    Update server.js
    Update package.json and lockfile dependencies

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from "./configs/db.js";
 import { inngest, functions } from "./inngest/index.js"
 import { serve } from "inngest/express";
 import { clerkWebhookHandler } from "./webhooks/clerk.js";
+import { clerkMiddleware } from "@clerk/express";
 
 
 
@@ -15,6 +16,8 @@ await connectDB();
 app.post('/api/webhooks/clerk', express.raw({ type: 'application/json' }), clerkWebhookHandler);
 app.use(express.json());
 app.use(cors());
+
+app.use(clerkMiddleware());
 
 app.get('/', (req, res) => res.send('Server is running!'));
 app.use('/api/inngest', serve({ client: inngest, functions: functions }));
